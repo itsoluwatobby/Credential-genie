05ab9cf9d626fbbe9e03f1c3d59e2f07828f68b8
itsoluwatobby <oluwasamuel050@gmail.com>
2023-12-03 06:07:24 +0100

    web5 issue resolved

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { CredentialDataProvider } from './context/CredentialContext.jsx'
 // import { disableReactDevTools } from '@fvilers/disable-react-devtools'
 
@@ -10,7 +11,11 @@ import { CredentialDataProvider } from './context/CredentialContext.jsx'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <CredentialDataProvider>
-      <App />
+      <BrowserRouter>
+        <Routes>
+          <Route path='/*' element={<App />} />
+        </Routes>
+      </BrowserRouter>
     </CredentialDataProvider>
   </React.StrictMode>,
 )
